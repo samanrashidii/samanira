297b4a44058b8a60d340196408df2204b5ed06fd
Saman Rashidi <Saman@edoramedia.com>
2017-04-20 17:31:11 +0430

    fix some issues

--- Website/js/js.js
+++ Website/js/js.js
@@ -1,5 +1,15 @@
 $(document).ready(function(){
 
+// Number Bullet
+
+$(window).load(function(){
+  var liNumber = $('.number-bullet li').length;
+  console.log(liNumber);
+  for(var i = 1; i <= liNumber; i++){
+    $('.number-bullet li:nth-child('+i+')').attr('list-number', i+' -');
+  }
+});  
+
 // Responsive Table
 
 $('.table.responsive').each(function(){  
@@ -59,11 +69,11 @@ $('form').validate({
     }
   },
   messages: {
-    nickname: "لطفا نام کاربری خود را وارد نمایید",
-    email_address: "لطفا ایمیل خود را وارد نمایید",
-    password: "لطفا رمز عبور خود را وارد نمایید",
-    confirm_password: "رمز عبور وارد شده با رمز عبور بالا مطابقت ندارد",
-    mobile: "لطفا شماره موبایل خود را وارد کنید"
+    nickname: "Please Enter Your Nickname",
+    email_address: "Please Enter Your Email Address",
+    password: "Please Enter Your Password",
+    confirm_password: "Please Confirm Your Password",
+    mobile: "Please Enter Your Mobile Number"
   },
   errorPlacement: function(error, element) {
     error.insertAfter(element);
