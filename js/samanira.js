24423c74fa9899847aed12b3ddffed83678a2949
Saman Rashidi <Saman@edoramedia.com>
2017-07-30 10:32:40 +0430

    fixed some indents

--- js/samanira.js
+++ js/samanira.js
@@ -18,15 +18,15 @@ var charLeft;
 $('.textarea-word-tracker textarea').on('keyup load', function () {
   charLeft = 250 - $(this).val().length;
   if (charLeft < 0) {
-      charLeft = 0;
+    charLeft = 0;
   }
   $(this).siblings('span').text(charLeft);
 });
 
 $(window).load( function() {
     $('.textarea-word-tracker textarea').each( function () {
-        charLeft = 250 - $(this).val().length;
-        $(this).siblings('span').text(charLeft);    
+      charLeft = 250 - $(this).val().length;
+      $(this).siblings('span').text(charLeft);    
     });
 });
 
@@ -117,7 +117,7 @@ var MessagePopup = function(){
 
 if($(window).width() > 980){
   $(function() {
-      $( ".ui-selectmenu" ).selectmenu();
+    $( ".ui-selectmenu" ).selectmenu();
   });
 }
 
@@ -189,38 +189,37 @@ $('form').validate({
 var fileUploaded = [];
 var gI = 0;
 window.onload = function(){   
-    if(window.File && window.FileList && window.FileReader)
-    {
-        $('#upload_bttn').on("change", function(event) {
-            var files = event.target.files;
-            if(files.length <= 10){
-              for(var i = 0; i < files.length; i++){
-                var file = files[i];
-                fileUploaded.push(file);
-                if(file.type.match('image.*')){
-                    if(this.files[0].size < 2097152){    
-                    var picReader = new FileReader();
-                    picReader.addEventListener("load",function(event){
-                        var picFile = event.target;
-                        if($('#uploaded-photos').children().length < 10){
-                           $('#uploaded-photos').append('<div class="uploaded-photo-box"><img src="' + picFile.result + '" /><a class="remove" href="javascript:void(0)" data-attr="rm_'+ gI++ +'">&nbsp;</a></div>'); 
-                        }            
-                    });
-                    picReader.readAsDataURL(file);
-                    }else{
-                      alert("Image Size is too big. Maximum size is 2MB.");
-                      $(this).val("");
-                    }
-                }else{
-                  alert("You can only upload image file.");
-                  $(this).val("");
-                }
+  if(window.File && window.FileList && window.FileReader){
+      $('#upload_bttn').on("change", function(event) {
+          var files = event.target.files;
+          if(files.length <= 10){
+            for(var i = 0; i < files.length; i++){
+              var file = files[i];
+              fileUploaded.push(file);
+              if(file.type.match('image.*')){
+                  if(this.files[0].size < 2097152){    
+                  var picReader = new FileReader();
+                  picReader.addEventListener("load",function(event){
+                      var picFile = event.target;
+                      if($('#uploaded-photos').children().length < 10){
+                         $('#uploaded-photos').append('<div class="uploaded-photo-box"><img src="' + picFile.result + '" /><a class="remove" href="javascript:void(0)" data-attr="rm_'+ gI++ +'">&nbsp;</a></div>'); 
+                      }            
+                  });
+                  picReader.readAsDataURL(file);
+                  }else{
+                    alert("Image Size is too big. Maximum size is 2MB.");
+                    $(this).val("");
+                  }
+              }else{
+                alert("You can only upload image file.");
+                $(this).val("");
               }
             }
-        });
-    }else{
-      console.log("Your browser does not support File API");
-    }
+          }
+      });
+  } else{
+    console.log("Your browser does not support File API");
+  }
 }
 
 // Uploaded Photo Remove
